da1906d00c1e39b2579245418317f12acde18848
Hugh Morrison <hmorri32@gmail.com>
2023-10-13 10:02:14 -0400

    install mapbox dependencies, loads env and touch sample.env

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Map from 'react-map-gl';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Map
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_API_KEY}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{ width: '100vw', height: '100vh' }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+  );
 }
 
-export default App
+export default App;
