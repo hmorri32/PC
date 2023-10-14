3f44c161e664658b9fbe28cf326191b6d9e825bc
Hugh Morrison <hmorri32@gmail.com>
2023-10-14 19:17:38 -0400

    create buffer migration

---
+++ backend/db/migrations/20231014210059_create_buffer.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('bufferData', (table) => {
+    table.increments('id').primary();
+    table.jsonb('data').notNull();
+    table
+      .integer('geojson_id')
+      .unsigned()
+      .references('id')
+      .inTable('geojson')
+      .unique()
+      .onDelete('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('bufferData');
+};
